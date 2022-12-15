15a7bbd921aacaa159c47d40df17c2601ccb3894
Debanjan Saha <debanjansh@gmail.com>
2022-12-14 19:25:23 -0500

    update files

--- InflationRecession/Codes/dashcore.py
+++ InflationRecession/Codes/dashcore.py
@@ -1,3 +1,5 @@
+#%%writefile dash_app.py
+
 import numpy as np
 import pandas as pd
 import plotly.express as px
@@ -14,81 +16,178 @@ df2 = df.merge(countries,
                left_on='level_0', 
                right_on='id',
                how='left')
+df_hvg = pd.read_csv(parent_dir + '/Data/hvg_data.csv')
+df_nvg = pd.read_csv(parent_dir + '/Data/nvg_data.csv')
+
 
 
 scl = [ [0,"rgb(5, 10, 172)"],[0.35,"rgb(40, 60, 190)"],[0.5,"rgb(70, 100, 245)"],\
     [0.6,"rgb(90, 120, 245)"],[0.7,"rgb(106, 137, 247)"],[1,"rgb(220, 220, 220)"] ]
 
-# fig = px.choropleth(filtered_df, locations="level_0",
-#                    color=overlay_choice,
-#                    hover_name='name',
-#                    color_continuous_scale=px.colors.sequential.Viridis,
-#                    animation_frame='level_1',
-#                    )
-
-# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+# Colors
+colors = {
+    'background': '#2f9fb3',
+    'graph_bg'  : '#aef6f7',
+    'title'     : '#f9f8f3',
+    'text'      : '#7a4d4a',
+    }
 
 app = Dash(__name__)
 
-app.layout = html.Div([
+app.layout = html.Div([    
     # Header
-    html.H1(children='Factors Influencing Inflation - Recession'),
-    html.H2(children='Select Indicator'),
-    # Dropdown
-    dcc.Dropdown(
-        id='overlay_choice',
-        options=indicator,
-        value='NY.GDP.MKTP.KD.ZG'
-    ),
+        html.Div([], className = 'col-0'), 
 
-   
-    dcc.Graph(id='graph', figure=fig),
+        html.Div([
+            html.H1(children='Factors Influencing Inflation on Various Economies',
+                    style = {'textAlign' : 'center', 
+                            'color' : colors['title'],
+                            'font-family': 'Helvetica'}
+            )],
+            className='col-1',
+            style = {'padding-top' : '1%'}
+        ),
 
-# fig = px.scatter_geo(df, locations="level_0",
-#                      color="continent", # which column to use to set the color of markers
-#                      hover_name="country", # column added to hover information
-#                      size="pop", # size of markers
-#                      projection="natural earth")
-# # fig = dict( data=data, layout=layout )    
+        html.Div([
+            html.H2(children='Select Indicator',
+            style = {'text-align' : 'left', 'color' : colors['text']}),
+            # Dropdown
+            html.Div([
+            dcc.Dropdown(
+                id='overlay_choice',
+                options=indicator,
+                value='NY.GDP.MKTP.KD.ZG',
+                style = {'font-family': 'Helvetica',
+                        'font-size': '13px', 
+                        'color' : colors['text'], 
+                        'white-space': 'nowrap', 
+                        'text-overflow': 'ellipsis'}
+                ),
+            ],style =  {'width' : '30%', 
+                        'margin-top' : '5px', 
+                        'margin-bottom' : '5px', 
+                        'text-align' : 'left', 
+                        'paddingLeft': 5,
+                        'color' : colors['graph_bg']}),
+        ], style = {'margin-top' : '5px', 
+                    'margin-bottom' : '5px', 
+                    'text-align' : 'left', 
+                    'paddingLeft': 25}
+        ),
 
-    
+        html.Div([
+        # World Map Graph
+        dcc.Graph(
+            id='graph1',
+            figure={
+                'layout': {
+                    'plot_bgcolor': colors['graph_bg'],
+                    'paper_bgcolor': colors['background'],
+                    'font': {
+                        'color': colors['text']}
+                    }
+            }),
+        ], style = {'margin-top' : '15px', 
+                    'margin-bottom' : '15px', 
+                    'text-align' : 'left', 
+                    'paddingLeft': 50,
+                    'paddingRight': 50,
+                    'background-color' : colors['background']}
+        ),
+        html.Div([
+        # Slider
+        dcc.Slider(
+            df2['level_1'].min(),
+            df2['level_1'].max(),
+            id='year_slider',
+            step=None,
+            value=df2['level_1'].min(),
+            marks={str(level_1): str(level_1) for level_1 in df2['level_1'].unique()}
+            
+        ),
+        ], style = {'margin-top' : '15px', 
+                    'margin-bottom' : '15px', 
+                    'text-align' : 'left', 
+                    'paddingLeft': 50,
+                    'paddingRight': 50,
+                    'background-color' : colors['background']}
+        ),
 
-
-    # Slider
-    # dcc.Graph(id='graph-with-slider'),
-    dcc.Slider(
-        df['level_1'].min(),
-        df['level_1'].max(),
-        id='year_slider',
-        step=None,
-        value=df['level_1'].min(),
-        marks={str(level_1): str(level_1) for level_1 in df['level_1'].unique()}
+        html.Div([
+            html.Div([
+                html.H2(children='Visibility Graph Network',
+                style = {'text-align' : 'left', 'paddingLeft': 25, 'color' : colors['text']}),
+            ], className = 'col-2'), 
+            # HVG & NVG Scatter Plots
+            html.Div([dcc.Graph(id='graph_hvg')], 
+                        style = {'margin-top' : '5px', 
+                            'margin-bottom' : '5px', 
+                            'text-align' : 'left', 
+                            'paddingLeft': 50,
+                            'paddingRight': 50,
+                            'background-color' : colors['background']}),
+            html.Div([dcc.Graph(id='graph_nvg')], 
+                        style = {'margin-top' : '5px', 
+                            'margin-bottom' : '5px', 
+                            'text-align' : 'left', 
+                            'paddingLeft': 50,
+                            'paddingRight': 50,
+                            'background-color' : colors['background']}),
+            
+            
+            ], style = {'margin-top' : '5px', 
+                        'margin-bottom' : '5px', 
+                        'text-align' : 'left', 
+                        'background-color' : colors['background']}
+        ),
         
+    ],
+    className = 'col-2',
+    style = {'paddingLeft': 10,
+            'paddingRight': 10,
+            'background-color' : colors['background']}
     )
-])
-
 
 ### Callback
 @app.callback(
-    Output('graph', 'figure'),
+    [Output('graph1', 'figure'),
+     Output('graph_hvg', 'figure'),
+     Output('graph_nvg', 'figure'),
+     ],
     [Input('overlay_choice', 'value'),
      Input('year_slider', 'value'),
      ])
 def update_figure(overlay_choice, year_slider):
-    filtered_df = df.loc[df['level_1'] == year_slider, overlay_choice]
+    filtered_df = df2.loc[df2['level_1'] == year_slider, ['level_0','level_1','iso2Code','name','region','longitude','latitude',overlay_choice]]
+    df_hvg_indicator = df_hvg[df_hvg['indicator code'] == overlay_choice]
+    df_nvg_indicator = df_nvg[df_nvg['indicator code'] == overlay_choice]
 
-    fig = px.choropleth(filtered_df, locations="level_0",
+    # plot map
+    fig1 = px.choropleth(filtered_df, 
+                        locations='level_0',
+                        locationmode = 'ISO-3',
+                        scope='world',
                         color=overlay_choice,
                         hover_name='name',
                         color_continuous_scale=px.colors.sequential.Viridis,
                         animation_frame='level_1',
+                        title='Indicator Distribution in World Map'
                         )
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig1.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     
-    return fig
+    # scatter plot HVG
+    fig2 = px.scatter(df_hvg_indicator, x="Average degree", y="Average path length", color='country',
+                    size='Network diameter', hover_data=['Network diameter'],
+                    title='Horizontal Visibility Graph by Countries')
+
+    # scatter plot NVG
+    fig3 = px.scatter(df_nvg_indicator, x="Average degree", y="Average path length", color='country',
+                 size='Network diameter', hover_data=['Network diameter'],
+                 title='Natural Visibility Graph by Countries')
+
+    # return the 3 graph objects
+    return fig1, fig2, fig3
 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
